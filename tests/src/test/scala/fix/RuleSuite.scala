c0dd4ad75195ebb424c0e8324b41f74fb01a8353
Alejandro Hernández <info@alejandrohdezma.com>
2023-08-30 10:15:45 +0200

    Run `sbt fix`

--- tests/src/test/scala/fix/RuleSuite.scala
+++ tests/src/test/scala/fix/RuleSuite.scala
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-import scalafix.testkit.AbstractSemanticRuleSuite
 import org.scalatest.funsuite.AnyFunSuiteLike
+import scalafix.testkit.AbstractSemanticRuleSuite
 
 class RuleSuite extends AbstractSemanticRuleSuite with AnyFunSuiteLike {
 
